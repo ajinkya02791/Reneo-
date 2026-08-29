e7dab9b5da24f42cdc452fc1f5dbc27717162e0f
ajinkya <77152422+ajinkya02791@users.noreply.github.com>
2026-08-29 16:46:47 +0000

    feat: add products context API around routes

--- reneo-f/src/App.tsx
+++ reneo-f/src/App.tsx
@@ -4,17 +4,21 @@ import Home from './Pages/Home'
 import ProductDetails from './Pages/ProductDetails'
 import CartPage from './Pages/Cart'
 import MainLayout from './Componenets/MainLayout'
+import Products from './Pages/Products'
+import { ProductsProvider } from './contextAPI/products'
 
 function App() {
 
   return (
     <BrowserRouter >
     <Routes>
-      
+    <Route element={<ProductsProvider />} >
     <Route element={<MainLayout />} >
     <Route path='/'  element={<Home />}/>
     <Route path='/products/:productID' element={<ProductDetails />} />
     <Route path='/cart' element={<CartPage />} />
+    <Route path='/products' element={<Products />} />
+    </Route>
     </Route>
     </Routes> 
     </BrowserRouter>
