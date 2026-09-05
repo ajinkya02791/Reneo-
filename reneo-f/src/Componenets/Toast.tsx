c7d9abe8549d9c4d43018a7f48277fd9d914492b
ajinkya <77152422+ajinkya02791@users.noreply.github.com>
2026-09-05 15:42:10 +0000

    refactor: change toast side, and apply animation

--- reneo-f/src/Componenets/Toast.tsx
+++ reneo-f/src/Componenets/Toast.tsx
@@ -1,27 +1,37 @@
 import type { ToastProps } from "../types/toast";
 
-export default function Toast({
-  message,
-  type = "success",
-}: ToastProps) {
-  const styles = {
+
+const toastStyles = {
     success: "bg-green-600",
     error: "bg-red-600",
     info: "bg-gray-800",
-  };
+};
 
-  return (
-    <div
-      className={`fixed bottom-5 left-5 z-50 flex items-center gap-2
-        rounded-lg px-4 py-3 text-sm text-white shadow-lg
-        ${styles[type]}
-        animate-in slide-in-from-left-5 duration-200`}
-    >
-      {type === "success" && "✓"}
-      {type === "error" && "✕"}
-      {type === "info" && "ⓘ"}
+export default function Toast({
+    message,
+    type = "success",
+}: ToastProps) {
+    return (
+        <div
+            className={`
+                fixed bottom-6 right-6 z-50
+                min-w-70 max-w-sm
+                rounded-xl px-5 py-4
+                text-base font-medium text-white
+                shadow-xl
+                ${toastStyles[type]}
+                animate-toast-in
+            `}
+        >
+            <div className="flex items-center gap-3">
+                <span className="text-lg">
+                    {type === "success" && "✓"}
+                    {type === "error" && "✕"}
+                    {type === "info" && "ⓘ"}
+                </span>
 
-      <span>{message}</span>
-    </div>
-  );
+                <span>{message}</span>
+            </div>
+        </div>
+    );
 }
