ae4a51720f0131375baea64b48e8aabc0726e86b
ajinkya <77152422+ajinkya02791@users.noreply.github.com>
2026-09-05 15:50:27 +0000

    feat: add Toast UI at the mainLayout

--- reneo-f/src/Componenets/MainLayout.tsx
+++ reneo-f/src/Componenets/MainLayout.tsx
@@ -1,12 +1,18 @@
 import Footer from './Footer'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
+import Toast from './Toast'
+import { useProducts } from '../contextAPI/products'
 
 function MainLayout() {
+
+  const { toast } = useProducts();
+
   return (
     <>
     <Header />
     <Outlet />
+    {toast && <Toast message={toast.message} type={toast.type} />}
     <Footer />
     </>
 
